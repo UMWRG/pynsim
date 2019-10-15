70bd78bbfb86ae8a84619edc1a7fb7173d211c40
knox <stephen.knox@manchester.ac.uk>
2019-10-15 17:12:24 +0100

    Change version to 0.1.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
         name = "PyNSim",
-        version = "0.1.4",
+        version = "0.1.5",
         author = "Stephen Knox",
         author_email = "stephen.knox@manchester.ac.uk",
         description = ("An generic simulator for water resources."),
