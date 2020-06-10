abf955dcad1344b1f4836197a2f200073422be2b
giovanni basolu <giovanni.basolu@manchester.ac.uk>
2020-06-10 16:43:42 +0100

    moved the code related to the tuple inside a new class

--- pynsim/multi_scenario/multi_dimensional_data.py
+++ pynsim/multi_scenario/multi_dimensional_data.py
@@ -7,6 +7,7 @@ import numpy as np
 import array
 # import pandas as pd
 import uuid
+import types
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -15,21 +16,88 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+
+class PropertyObjectData(object):
+    """
+        This class represents the single object identified by the tuple (obj type, obj name, property name)
+    """
+    def __init__(self, **kwargs):
+        self._structure = types.SimpleNamespace()
+
+        self._structure.object_type = None
+        self._structure.object_name = None
+        self._structure.object_reference = None
+        self._structure.property_name = None
+        self._structure.property_data = []
+
+        self.set_data(**kwargs)
+
+    def set_data(self, object_reference=None, object_type=None, object_name=None, property_name=None, property_data=None):
+        """
+            Used to add data to the structure, that can be an array-like or a single value
+        """
+        if object_type is None:
+            raise Exception("The data type has to be associated with a valid object type!")
+        self._structure.object_type = object_type
+
+        self._structure.property_name = property_name
+        if isinstance(property_data, (list,array.array,range,np.ndarray)):
+            self._structure.property_data = property_data
+        else:
+            self._structure.property_data = [property_data]
+
+        if object_name is None:
+            # Generating an UUID for the name
+            raise Exception("The data name has to be associated with a valid object name!")
+
+        self._structure.object_name = object_name
+
+        # We need an unique reference to every column
+        self._structure.object_reference = object_reference
+
+
+
+    def get_data(self, format = "simple", index=0):
+        if format=="simple":
+            return self._structure.property_data[index]
+        elif format=="full":
+            return {
+                "type" : self._structure.object_type,
+                "name" : self._structure.object_name,
+                "reference" : self._structure.object_reference,
+                "property_name": self._structure.property_name,
+                "property_data": self._structure.property_data[index]
+            }
+        else:
+            raise Exception(f"The format {format} is not allowed!")
+
+    def data_length(self):
+        return len(self._structure.property_data)
+
+
 class MultiDimensionalData(object):
     """
         This class allows to enumerate all the data combinations in a lazy style.
         Every combination is built upon its request by the client
     """
-    _references=[] # Array containing the referneces to the objects associated to every column
-    _names=[] # Array containing the names associated to every column
-    _data=[] # Array containing the data associated to every column
-    _indexes=[] # Array containing the current index associated to every column
-    _iteration_finished=False
-    _iterations_counter=0
-    _output_format = "simple"
 
     def __init__(self):
         logger.info("MultiDimensionalData INIT")
+
+        # self._references=[] # Array containing the referneces to the objects associated to every column
+        # self._names=[] # Array containing the names associated to every column
+        # self._data=[] # Array containing the data associated to every column
+        # self._types=[] # Array containing the current type associated to every column
+
+        self._objects_map = {}
+        self._objects_list = []
+        self._indexes=[] # Array containing the current index associated to every column
+
+        self._iteration_finished=False
+        self._iterations_counter=0
+        self._output_format = "simple"
+
+
         self._iterations_counter=1
 
     def __iter__(self):
@@ -65,24 +133,39 @@ class MultiDimensionalData(object):
             raise Exception(f"The format {format} is not allowed!")
         self._output_format = format
 
-    def add_data(self, name=None, reference=None, data=None):
-        """
-            Used to add data to the structure, that can be an array-like or a single value
-        """
-        if isinstance(data, (list,array.array,range,np.ndarray)):
-            self._data.append(data)
-        else:
-            self._data.append([data])
+
+    def add_data(self, object_reference=None, object_type=None, object_name=None, property_name=None, property_data=None):
+        unique_id = f"{object_type},{object_name},{property_name}"
+        if unique_id in self._objects_map:
+            raise Exception(f"The tuple ({unique_id}) has been already defined!")
+
+        new_object = PropertyObjectData(object_reference=object_reference, object_type=object_type, object_name=object_name, property_name=property_name, property_data=property_data)
+        self._objects_map[unique_id] = new_object
         self._indexes.append(0)
+        self._objects_list.append(new_object)
 
-        if name is None:
-            # Generating an UUID for the name
-            name = str(uuid.uuid4())
-
-        self._names.append(name)
-
-        # We need an unique reference to every column
-        self._references.append(reference)
+    # def add_data_old(self, type=None, name=None, reference=None, data=None):
+    #     """
+    #         Used to add data to the structure, that can be an array-like or a single value
+    #     """
+    #     if type is None:
+    #         raise Exception("The data type has to be associated with a valid type!")
+    #
+    #     if isinstance(data, (list,array.array,range,np.ndarray)):
+    #         self._data.append(data)
+    #     else:
+    #         self._data.append([data])
+    #     self._indexes.append(0)
+    #
+    #     if name is None:
+    #         # Generating an UUID for the name
+    #         name = str(uuid.uuid4())
+    #
+    #     self._names.append(name)
+    #     self._types.append(type)
+    #
+    #     # We need an unique reference to every column
+    #     self._references.append(reference)
 
 
     def get_next_data(self):
@@ -106,16 +189,18 @@ class MultiDimensionalData(object):
         data_index = self.get_current_index()
         next_data = []
         for col_count, index in enumerate(data_index):
-            if self._output_format=="simple":
-                next_data.append(self._data[col_count][index])
-            elif self._output_format=="full":
-                next_data.append({
-                    "name" : self._names[col_count],
-                    "reference" : self._references[col_count],
-                    "data": self._data[col_count][index]
-                })
-            else:
-                raise Exception(f"The format {self._output_format} is not allowed!")
+            next_data.append(self._objects_list[col_count].get_data(index=index, format="full"))
+            # if self._output_format=="simple":
+            #     next_data.append(self._data[col_count][index])
+            # elif self._output_format=="full":
+            #     next_data.append({
+            #         "type" : self._types[col_count],
+            #         "name" : self._names[col_count],
+            #         "reference" : self._references[col_count],
+            #         "data": self._data[col_count][index]
+            #     })
+            # else:
+            #     raise Exception(f"The format {self._output_format} is not allowed!")
 
         if increment_index is True:
             self._increment_index()
@@ -132,8 +217,10 @@ class MultiDimensionalData(object):
 
         flag_update_index_done = False
 
-        for main_list_index, datum  in reversed(list(enumerate(self._data))):
-            datum_length=len(datum)
+        #        for main_list_index, datum  in reversed(list(enumerate(self._data))):
+        for main_list_index, obj  in reversed(list(enumerate(self._objects_list))):
+
+            datum_length=obj.data_length()
             current_datum_index=self._indexes[main_list_index]
 
             if flag_update_index_done is False:
