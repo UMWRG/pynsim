632dc8aabc70c0af889dfeb716d0600550d0b639
giovanni basolu <giovanni.basolu@manchester.ac.uk>
2020-06-17 12:21:05 +0100

    exporting property data

--- pynsim/multi_scenario/multi_dimensional_data.py
+++ pynsim/multi_scenario/multi_dimensional_data.py
@@ -5,9 +5,7 @@
 import logging
 import numpy as np
 import array
-# import pandas as pd
 import uuid
-import types
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -15,65 +13,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-
-
-class PropertyObjectData(object):
-    """
-        This class represents the single object identified by the tuple (obj type, obj name, property name)
-    """
-    def __init__(self, **kwargs):
-        self._structure = types.SimpleNamespace()
-
-        self._structure.object_type = None
-        self._structure.object_name = None
-        self._structure.object_reference = None
-        self._structure.property_name = None
-        self._structure.property_data = []
-
-        self.set_data(**kwargs)
-
-    def set_data(self, object_reference=None, object_type=None, object_name=None, property_name=None, property_data=None):
-        """
-            Used to add data to the structure, that can be an array-like or a single value
-        """
-        if object_type is None:
-            raise Exception("The data type has to be associated with a valid object type!")
-        self._structure.object_type = object_type
-
-        self._structure.property_name = property_name
-        if isinstance(property_data, (list,array.array,range,np.ndarray)):
-            self._structure.property_data = property_data
-        else:
-            self._structure.property_data = [property_data]
-
-        if object_name is None:
-            # Generating an UUID for the name
-            raise Exception("The data name has to be associated with a valid object name!")
-
-        self._structure.object_name = object_name
-
-        # We need an unique reference to every column
-        self._structure.object_reference = object_reference
-
-
-
-    def get_data(self, format = "simple", index=0):
-        if format=="simple":
-            return self._structure.property_data[index]
-        elif format=="full":
-            return {
-                "type" : self._structure.object_type,
-                "name" : self._structure.object_name,
-                "reference" : self._structure.object_reference,
-                "property_name": self._structure.property_name,
-                "property_data": self._structure.property_data[index]
-            }
-        else:
-            raise Exception(f"The format {format} is not allowed!")
-
-    def data_length(self):
-        return len(self._structure.property_data)
-
+from .property_object_data import PropertyObjectData
 
 class MultiDimensionalData(object):
     """
@@ -81,15 +21,11 @@ class MultiDimensionalData(object):
         Every combination is built upon its request by the client
     """
 
-    def __init__(self):
+    def __init__(self, save_components_history = True):
         logger.info("MultiDimensionalData INIT")
 
-        # self._references=[] # Array containing the referneces to the objects associated to every column
-        # self._names=[] # Array containing the names associated to every column
-        # self._data=[] # Array containing the data associated to every column
-        # self._types=[] # Array containing the current type associated to every column
+        self._objects_map = {} # Map of the indexes used for all the oject indexed ty the tuple (object type, object name, property name)
 
-        self._objects_map = {}
         self._objects_list = []
         self._indexes=[] # Array containing the current index associated to every column
 
@@ -97,8 +33,9 @@ class MultiDimensionalData(object):
         self._iterations_counter=0
         self._output_format = "simple"
 
-
         self._iterations_counter=1
+
+        self._save_components_history = save_components_history
 
     def __iter__(self):
         """
@@ -128,49 +65,47 @@ class MultiDimensionalData(object):
             format:
                 "simple" => returns the array of data
                 "full" => return the array of objects: { "name": "<name of column>", "reference": "<reference of column>", "data": <the data>}
+                "tree" => returns a tree organized by object_type.object_name.property_name
         """
-        if format is not "simple" and format is not "full":
+        if format is not "simple" and format is not "full" and format is not "tree":
             raise Exception(f"The format {format} is not allowed!")
         self._output_format = format
 
 
+    def get_tuple_index(self, object_type=None, object_name=None, property_name=None):
+        """
+            Returns the tuple:
+                ( object_type, object_name, property_name )
+        """
+        if object_type is None:
+            raise Exception("The Object Type cannot be None")
+
+        if object_name is None:
+            raise Exception("The Object Name cannot be None")
+
+        if property_name is None:
+            raise Exception("The Property Name cannot be None")
+
+        return f"{object_type.strip().lower()},{object_name.strip().lower()},{property_name.strip().lower()}"
+
     def add_data(self, object_reference=None, object_type=None, object_name=None, property_name=None, property_data=None):
-        unique_id = f"{object_type},{object_name},{property_name}"
+        """
+            Add data related to the tuple
+                ( object_type, object_name, property_name )
+        """
+        unique_id = self.get_tuple_index(object_type, object_name, property_name)
         if unique_id in self._objects_map:
             raise Exception(f"The tuple ({unique_id}) has been already defined!")
 
         new_object = PropertyObjectData(object_reference=object_reference, object_type=object_type, object_name=object_name, property_name=property_name, property_data=property_data)
-        self._objects_map[unique_id] = new_object
+        # self._objects_map[unique_id] = new_object
+        self._objects_map[unique_id] = len(self._objects_list)
         self._indexes.append(0)
         self._objects_list.append(new_object)
 
-    # def add_data_old(self, type=None, name=None, reference=None, data=None):
-    #     """
-    #         Used to add data to the structure, that can be an array-like or a single value
-    #     """
-    #     if type is None:
-    #         raise Exception("The data type has to be associated with a valid type!")
-    #
-    #     if isinstance(data, (list,array.array,range,np.ndarray)):
-    #         self._data.append(data)
-    #     else:
-    #         self._data.append([data])
-    #     self._indexes.append(0)
-    #
-    #     if name is None:
-    #         # Generating an UUID for the name
-    #         name = str(uuid.uuid4())
-    #
-    #     self._names.append(name)
-    #     self._types.append(type)
-    #
-    #     # We need an unique reference to every column
-    #     self._references.append(reference)
-
-
     def get_next_data(self):
         """
-            Gets the next index (not yet requested) and returns the associated data
+            Gets the current indexed data
             The index is incremented
         """
         data = self.get_current_data(increment_index=True)
@@ -178,6 +113,18 @@ class MultiDimensionalData(object):
         return data
 
     #######################################################################################
+    def get_current_object_property_data(self, object_type=None, object_name=None, property_name=None):
+        """
+            Returns the current scenario data for the provided object/property
+        """
+        unique_id = self.get_tuple_index(object_type, object_name, property_name)
+        if unique_id not in self._objects_map:
+            raise Exception(f"The tuple ({unique_id}) has not been yet defined!")
+
+
+        object_property_all_data_mgr = self._objects_list[ self._objects_map[unique_id] ]
+        object_scenario_index = self._indexes[ self._objects_map[unique_id] ]
+        return object_property_all_data_mgr.get_data(index=object_scenario_index)
 
     def get_current_data(self, increment_index=False):
         """
@@ -187,25 +134,43 @@ class MultiDimensionalData(object):
                 True  => The index is incremented
         """
         data_index = self.get_current_index()
-        next_data = []
+
+        if self._output_format == "tree":
+            current_data = {}
+        else:
+            current_data = []
+
         for col_count, index in enumerate(data_index):
-            next_data.append(self._objects_list[col_count].get_data(index=index, format="full"))
-            # if self._output_format=="simple":
-            #     next_data.append(self._data[col_count][index])
-            # elif self._output_format=="full":
-            #     next_data.append({
-            #         "type" : self._types[col_count],
-            #         "name" : self._names[col_count],
-            #         "reference" : self._references[col_count],
-            #         "data": self._data[col_count][index]
-            #     })
-            # else:
-            #     raise Exception(f"The format {self._output_format} is not allowed!")
+            # current_data.append(self._objects_list[col_count].get_data(index=index, format="full"))
+            item_data = self._objects_list[col_count].get_data(index=index, format=self._output_format)
+            if self._output_format == "tree":
+                """
+                    In this case the full data are returned as a tree
+                """
+                if not item_data["object_type"] in current_data:
+                    current_data[item_data["object_type"]] = {}
+                if not item_data["object_name"] in current_data[item_data["object_type"]]:
+                    current_data[item_data["object_type"]][item_data["object_name"]] = {
+                        "reference": item_data["object_reference"],
+                        "properties": {}
+                    }
+                if not item_data["property_name"] in current_data[item_data["object_type"]][item_data["object_name"]]["properties"]:
+                    current_data[item_data["object_type"]][item_data["object_name"]]["properties"][item_data["property_name"]] = {
+                        "data"      : item_data["property_data"],
+                        # "reference" : item_data["object_reference"],
+                        "current_index" : item_data["current_index"]
+                    }
+                else:
+                    raise Exception("Error")
+            else:
+                current_data.append(item_data)
+
+        index_tuple = self.get_current_index_tuple()
 
         if increment_index is True:
             self._increment_index()
 
-        return next_data
+        return {"data": current_data, "index": data_index, "tuple": index_tuple}
 
 
     def _increment_index(self):
@@ -216,6 +181,11 @@ class MultiDimensionalData(object):
             raise StopIteration
 
         flag_update_index_done = False
+
+        logger.info("CURRENT VALUE")
+        for index, index_value in enumerate(self._indexes):
+            logger.info("index: %s, index_value: %s", index, index_value)
+            logger.info("Object data %s", self._objects_list[index].get_current_indexed_data())
 
         #        for main_list_index, datum  in reversed(list(enumerate(self._data))):
         for main_list_index, obj  in reversed(list(enumerate(self._objects_list))):
@@ -237,6 +207,18 @@ class MultiDimensionalData(object):
         self._iteration_finished = not flag_update_index_done # In case that it has not been possible incrementing the index => we finished the possible indexes
         self._iterations_counter = self._iterations_counter + 1
 
+        for index, index_value in enumerate(self._indexes):
+            # The index inside every object is updated
+            self._objects_list[index].set_current_index(index_value)
+
+    def get_current_index_tuple(self):
+        """
+            Returns the current index as a tuple in order to be used as index of a dict, if available, StopIteration otherwise
+        """
+        if self._iteration_finished:
+            raise StopIteration
+        return ','.join([str(x) for x in self._indexes])
+
     def get_current_index(self):
         """
             Returns the current index, if available, StopIteration otherwise
@@ -250,40 +232,3 @@ class MultiDimensionalData(object):
             Returns the current iteration count
         """
         return self._iterations_counter
-
-    #################################################
-
-    # def get_next_index(self):
-    #     """
-    #         Returns the first not used index
-    #     """
-    #
-    #     if self._iteration_finished:
-    #         raise StopIteration
-    #
-    #     flag_update_index_done = False
-    #
-    #     _new_index=self._indexes.copy()
-    #
-    #     for main_list_index, datum  in reversed(list(enumerate(self._data))):
-    #         # print(f"datum {datum}")
-    #         # print(f"main_list_index {main_list_index}")
-    #         datum_length=len(datum)
-    #         current_datum_index=self._indexes[main_list_index]
-    #
-    #         # print(f"datum_length: {datum_length} current_datum_index: {current_datum_index}")
-    #         if flag_update_index_done is False:
-    #             # I have to check the current index
-    #             if current_datum_index < ( datum_length - 1 ):
-    #                 # Increment the index for that column
-    #                 self._indexes[main_list_index] = self._indexes[main_list_index] + 1
-    #                 flag_update_index_done = True
-    #                 break
-    #             else:
-    #                 # Column index overflow
-    #                 self._indexes[main_list_index] = 0
-    #
-    #     self._iteration_finished = not flag_update_index_done
-    #     self._iterations_counter = self._iterations_counter + 1
-    #
-    #     return _new_index
