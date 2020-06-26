da80377b7d8bc125d90f82653a27ef0bd6201910
giovanni basolu <giovanni.basolu@manchester.ac.uk>
2020-06-26 19:04:28 +0100

    removed prints

--- examples/simple_routing_multi_scenario/engines/routing.py
+++ examples/simple_routing_multi_scenario/engines/routing.py
@@ -41,20 +41,20 @@ class SimpleRouting(Engine):
         # Set initial storage for this simulation time step
         init_stor = dict()
         for res in node_types['Reservoir']:
-            print("self.target.nodes[res]._history")
-            print(self.target.nodes[res]._history)
+            # print("self.target.nodes[res]._history")
+            # print(self.target.nodes[res]._history)
             if "S" not in self.target.nodes[res]._history or \
                 len(self.target.nodes[res]._history['S']) == 0:
                 init_stor[res] = self.target.nodes[res].init_stor
                 # init_stor[res] = self.target.nodes[res].get_current_history_value("init_stor")
 
-                print(f"1 - init_stor {res} {init_stor[res]}")
+                # print(f"1 - init_stor {res} {init_stor[res]}")
             else:
                 # init_stor[res] = self.target.nodes[res]._history['S'][-1]
                 init_stor[res] = self.target.nodes[res].get_previous_history_value("S")
-                print(f"2 - init_stor {res} {init_stor[res]}")
+                # print(f"2 - init_stor {res} {init_stor[res]}")
 
-        print(init_stor)
+        # print(init_stor)
         # input("init_stor")
         # Iteratively calclate releases and storages
 
