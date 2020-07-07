a8a9f84c009c8603a085c4c001fd299888b4972a
giovanni basolu <giovanni.basolu@manchester.ac.uk>
2020-07-07 22:05:20 +0100

    new

---
+++ examples/simple_routing_multi_scenario/engines/show_status.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*
+
+from pynsim import Engine
+
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s:%(levelname)s:%(name)s:%(lineno)s:%(message)s"
+)
+logger = logging.getLogger(__name__)
+
+class ShowStatus(Engine):
+    """
+        This engine just shows the status of the iteration
+    """
+
+    name = "Show status"
+    target = None
+
+    def run(self):
+        """
+            execute the show
+        """
+        if self.simulator.loop_priority == "scenario":
+            logger.warning("This is the timestep {} of the scenario {}".format(self.simulator.current_timestep, self.simulator.current_scenario_id))
+        elif self.simulator.loop_priority == "timestep":
+            logger.warning("This is the scenario {} of the timestep {}".format( self.simulator.current_scenario_id, self.simulator.current_timestep))
